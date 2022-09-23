652ae724904da9e84b800c5f5455df9e8ad4e33a
Ray <68147363+RayCatYT@users.noreply.github.com>
2022-09-23 20:43:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,21 @@ def help(resp):
     if resp.event.message:
         message = resp.parsed.auto()
         if message['content'].startswith('me.help'):
-            username = m['author']['username']
+            username = message['author']['username']
             if username == "Azazel":
                 bot.sendMessage(message['channel_id'], 'bro just look at the code its not that hard... its on the discord if u dumbass deleted it')
             print(username + "used help command")
             bot.sendMessage(message['channel_id'], username)
             
+@bot.gateway.command
+def owohnb(resp):
+    if resp.event.message:
+        message = resp.parsed.auto()
+        if message['content'].startswith('owo h'):
+            username = message['author']['username']
+            if username == "Azazel":
+                bot.sendMessage(message['channel_id'], 'owo b')
+            print(username + "used owo hnb :)")
+            bot.sendMessage(message['channel_id'], username)
 
 bot.gateway.run(auto_reconnect=True)
