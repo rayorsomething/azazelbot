9f240a7c93fd12da8b55549e194211fd39ccf621
Ray <68147363+RayCatYT@users.noreply.github.com>
2022-09-23 20:41:10 +0200

    azazel bo tuuuuuuuuuuuuuuuuuuuuuuuuuuup

---
+++ main.py
@@ -0,0 +1,22 @@
+import discum   
+import os
+import time
+from discum.utils.slash import SlashCommander
+from discum.utils.embed import Embedder
+
+TOKEN = os.environ.get("DISCORD_TOKEN")
+bot=discum.Client(token=TOKEN)
+
+@bot.gateway.command
+def help(resp):
+    if resp.event.message:
+        message = resp.parsed.auto()
+        if message['content'].startswith('me.help'):
+            username = m['author']['username']
+            if username == "Azazel":
+                bot.sendMessage(message['channel_id'], 'bro just look at the code its not that hard... its on the discord if u dumbass deleted it')
+            print(username + "used help command")
+            bot.sendMessage(message['channel_id'], username)
+            
+
+bot.gateway.run(auto_reconnect=True)
